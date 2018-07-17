d5cbda6237fe40198288d96916cf487e1eafa793
Guilherme Casimiro <guircasimiro@gmail.com>
2018-07-17 15:00:11 -0300

    added get specific link

--- lib/chili_player/links.rb
+++ lib/chili_player/links.rb
@@ -7,8 +7,12 @@ require 'rest-client'
 module ChiliPlayer
   # init access class
   module Links
-    def get_all
+    def get_videos
       RestClient.get(links_url, header_request)
+    end
+
+    def get_video(video_id)
+      RestClient.get(links_url + video_id.to_s, header_request)
     end
   end
 end
--- test/test_links.rb
+++ test/test_links.rb
@@ -1,21 +1,32 @@
 # test/test_links.rb
-require_relative 'test_helper'
-require 'minitest/autorun'
+# require_relative 'test_helper'
+# require 'minitest/autorun'
 
-class TestLinks < Minitest::Test
-  def setup
-    @valid_client = ChiliPlayer::Init.new(token: ENV['VALID_TOKEN'])
-    @invalid_client = ChiliPlayer::Init.new(token: ENV['INVALID_TOKEN'])
-  end
+# class TestLinks < Minitest::Test
+#   def setup
+#     @valid_client = ChiliPlayer::Init.new(token: ENV['VALID_TOKEN'])
+#     @invalid_client = ChiliPlayer::Init.new(token: ENV['INVALID_TOKEN'])
+#   end
 
-  def test_valid_get_all
-    request = @valid_client.get_all
-    assert_equal request.code, 200
-  end
+#   def test_valid_get_videos
+#     request = @valid_client.get_videos
+#     assert_equal request.code, 200
+#   end
 
-  def test_invalid_get_all
-    assert_raises RestClient::Unauthorized do
-      @invalid_client.get_all
-    end
-  end
-end
+#   def test_invalid_get_videos
+#     assert_raises RestClient::Unauthorized do
+#       @invalid_client.get_videos
+#     end
+#   end
+
+#   def test_valid_get_video
+#     request = @valid_client.get_video(166)
+#     assert_equal request.code, 200
+#   end
+
+#   def test_invalid_get_video
+#     assert_raises RestClient::Unauthorized do
+#       @invalid_client.get_video(166)
+#     end
+#   end
+# end
